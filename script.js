03211a5a58ca60eb5808b3bcea4dabb0283e3649
paulohldev <paulo85542080@gmail.com>
2022-10-21 00:41:55 -0300

    preenchedor de cep com javascript

---
+++ script.js
@@ -0,0 +1,20 @@
+const inputs = document.querySelectorAll('input[type="text"]');
+
+function trocaCep() {
+  let cepChange = "";
+  cepChange = inputs[0].value;
+  buscaCep(cepChange);
+}
+
+async function buscaCep(cepRequest) {
+  const dadosCep = await fetch(`https://viacep.com.br/ws/${cepRequest}/json/`);
+  const dadosJSON = await dadosCep.json();
+
+  const { uf: estado, localidade: cidade, bairro, logradouro: rua } = dadosJSON;
+
+  inputs[1].value = estado;
+  inputs[2].value = cidade;
+  inputs[3].value = bairro;
+  inputs[4].value = rua;
+}
+inputs[0].addEventListener("change", trocaCep);
