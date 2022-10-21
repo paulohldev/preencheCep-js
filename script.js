d3eee6359a19488d0a4f3bacdf5961ed9ab5b4f7
paulohldev <paulo85542080@gmail.com>
2022-10-21 00:47:19 -0300

    try/catch

--- script.js
+++ script.js
@@ -7,14 +7,25 @@ function trocaCep() {
 }
 
 async function buscaCep(cepRequest) {
-  const dadosCep = await fetch(`https://viacep.com.br/ws/${cepRequest}/json/`);
-  const dadosJSON = await dadosCep.json();
+  try {
+    const dadosCep = await fetch(
+      `https://viacep.com.br/ws/${cepRequest}/json/`
+    );
+    const dadosJSON = await dadosCep.json();
 
-  const { uf: estado, localidade: cidade, bairro, logradouro: rua } = dadosJSON;
+    const {
+      uf: estado,
+      localidade: cidade,
+      bairro,
+      logradouro: rua,
+    } = dadosJSON;
 
-  inputs[1].value = estado;
-  inputs[2].value = cidade;
-  inputs[3].value = bairro;
-  inputs[4].value = rua;
+    inputs[1].value = estado;
+    inputs[2].value = cidade;
+    inputs[3].value = bairro;
+    inputs[4].value = rua;
+  } catch (err) {
+    console.log(err);
+  }
 }
 inputs[0].addEventListener("change", trocaCep);
